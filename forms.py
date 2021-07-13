131921d76cbc6e3227748f64cf20e479371d77a0
Yarosalv Adashian <Adashyan94@ukr.net>
2021-07-13 15:28:53 +0300

    add validators and message for amount field

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import SelectField, SubmitField, TextAreaField, FloatField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, NumberRange, Length
 
 
 class PaidForm(FlaskForm):
     ''' Формы для html страницы'''
-    amount = FloatField('Сумма', validators=[InputRequired(message='Необходимо заполнить')])
+    amount = FloatField('Сумма', validators=[InputRequired(), NumberRange(0.1, 9999)])
     currency = SelectField('Валюта', choices=[(978, 'EUR'), (840, 'USD'), (643, 'RUB')])
     description_field = TextAreaField('Описание товара', [Length(min=0, max=4096)])
     submit = SubmitField('Оплатить')
